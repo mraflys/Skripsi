1ba08f7b651a572396bcb9bb7d136a0b6f4a47cc
Rafly <raflyssyahputra@gmail.com>
2022-04-09 20:43:40 +0700

    Auth Fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,8 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
-
+    public const REGISTER = '/';
+    public const LOGIN = '/home';
     /**
      * Define your route model bindings, pattern filters, etc.
      *
